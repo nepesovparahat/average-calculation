1aadafff8946e318d08ee0f46a39749acf46c924
Parahat <nepesovparahatt@gmail.com>
2021-04-11 22:28:16 +0300

    Added a row data to json

--- README.md
+++ README.md
@@ -1,23 +1,13 @@
-# vuejs-playlist
-
-> A Vue.js project
+### Vue Avarage Calculation
 
 ## Build Setup
 
-``` bash
-# install dependencies
-npm install
+```sh
+$ cd Vue-Avarage-Calculation
+$ npm install
 
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
+$ npm run dev
 ```
-
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
-
-
 ### Program made with vue that calculates the student's GPA.
 Project open source link: https://thirsty-brattain-740cf5.netlify.app/
-![ezgif com-video-to-gif](https://user-images.githubusercontent.com/67821216/91900027-b5f9e180-eca6-11ea-8b88-ff6e3358fdb5.gif)
+
--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,52 @@
 <template>
   <div>
-    <h2>Name:{{name}}</h2>
-  <h2>ID:{{year}}</h2>
-
-   <ap-hom v-bind:students="students"></ap-hom>
- 
-  </div>  
+    <h2>Name:{{ name }}</h2>
+    <h2>ID:{{ year }}</h2>
+    <ap-hom v-bind:students="students"></ap-hom>
+  </div>
 </template>
 
 <script>
-
-
-import Home from './components/Home.vue'
-
-
+import Home from "./components/Home.vue";
 
 export default {
-  components:{'ap-hom':Home},
-  data () {
+  components: { "ap-hom": Home },
+  data() {
     return {
-     
-       students:[{
-         code:"AB123k",
-         firstgrade:60,
-         twograde:50,
-         finalgrade:90
-       },
-       {
-         code:"KG563c",
-         firstgrade:70,
-         twograde:98,
-         finalgrade:65
-       },
-       {
-         code:"KL432u",
-         firstgrade:45,
-         twograde:90,
-         finalgrade:45
-       }
-       ],
-       year: "456923012",
-       name: 'Parahat Nepesov'
-        
-    }
-  }
-}
+      students: [
+        {
+          code: "AB123k",
+          firstgrade: 60,
+          twograde: 50,
+          finalgrade: 90,
+        },
+        {
+          code: "KG563c",
+          firstgrade: 70,
+          twograde: 98,
+          finalgrade: 65,
+        },
+        {
+          code: "KL432u",
+          firstgrade: 45,
+          twograde: 90,
+          finalgrade: 45,
+        },
+        {
+          code: "KL432u",
+          firstgrade: 89,
+          twograde: 97,
+          finalgrade: 100,
+        },
+      ],
+      year: "456923012",
+      name: "Parahat Nepesov",
+    };
+  },
+};
 </script>
-
 <style scoped>
-
 h1 {
-  color:purple;
+  color: purple;
 }
-
-
-
 </style>
